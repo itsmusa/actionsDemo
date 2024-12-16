a86c76e24b41c6dabe5faa083247ba9aa5af3fa2
itsmusa <musamagwaza23@protonmail.com>
2024-12-16 12:04:29 +0200

    Testing pushing

--- file.js
+++ file.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const fs = require('fs');
+
+// Data to write
+const data = "Hello, this is a sample text!";
+
+// Write to a file (creates a file if it doesn't exist, or overwrites it)
+fs.writeFile('sample.txt', data, (err) => {
+  if (err) {
+    console.log("Error writing to file", err);
+  } else {
+    console.log("File written successfully!");
+  }
+});
