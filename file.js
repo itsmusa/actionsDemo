dcbbf9222cd1fbf75016bf7ec512022db15ae46c
itsmusa <musamagwaza23@protonmail.com>
2024-12-17 19:43:44 +0200

    testing push again

--- file.js
+++ file.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 // Data to write
-const data = "Hello, this is a sample text!";
+const data = "Hello, this is another sample text push example!";
 
 // Write to a file (creates a file if it doesn't exist, or overwrites it)
-fs.writeFile('sample.txt', data, (err) => {
+fs.writeFile('sample2.txt', data, (err) => {
   if (err) {
     console.log("Error writing to file", err);
   } else {
